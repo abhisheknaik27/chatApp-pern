941c92cdb1a6390033b2171d1ed053d482adaf56
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-11-28 13:39:44 +0530

    sidebar component ui

--- client/src/components/sidebar/LogoutButton.tsx
+++ client/src/components/sidebar/LogoutButton.tsx
@@ -1,5 +1,11 @@
+import { TbLogout2 } from "react-icons/tb";
+
 const LogoutButton = () => {
-  return <div></div>;
+  return (
+    <div className="mt-auto pl-2">
+      <TbLogout2 className="text-2xl text-blue-950 cursor-pointer" />
+    </div>
+  );
 };
 
 export default LogoutButton;
--- client/src/components/sidebar/SearchInput.tsx
+++ client/src/components/sidebar/SearchInput.tsx
@@ -1,5 +1,18 @@
+import { IoSearch } from "react-icons/io5";
+
 const SearchInput = () => {
-  return <div></div>;
+  return (
+    <form className="flex items-center gap-2">
+      <input
+        type="text"
+        placeholder="Search"
+        className="input input-bordered rounded-full text-lg pl-6"
+      />
+      <button type="submit" className="btn btn-circle bg-sky-500 text-white">
+        <IoSearch className="text-lg outline-none" />
+      </button>
+    </form>
+  );
 };
 
 export default SearchInput;
