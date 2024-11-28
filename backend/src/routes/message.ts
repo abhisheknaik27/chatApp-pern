9083aad4def5785fd15766f46d15d92f61e5ea7f
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-11-28 10:53:36 +0530

    message routes

--- backend/src/routes/message.ts
+++ backend/src/routes/message.ts
@@ -1,8 +1,11 @@
-import express from "express";
+import express, { RequestHandler } from "express";
+import protectRoute from "../middleware/protectRoute.js";
+import { getMessage, sendMessage, getUsersForSidebar } from "../controllers/messageController.js";
 const router = express.Router();
 
-router.get('/conversations', (req, res) => {
-    res.send('all conversation');
-});
+
+router.get('/conversations', protectRoute as RequestHandler, getUsersForSidebar);
+router.post('/send/:id', protectRoute as RequestHandler, sendMessage);
+router.get('/:id', protectRoute as RequestHandler, getMessage as RequestHandler);
 
 export default router;
