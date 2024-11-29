13175d5c604d93c34c453728022508e49154fe63
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-11-29 12:29:42 +0530

    login functionality added

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import { FaUser } from "react-icons/fa";
 import { FaKey } from "react-icons/fa6";
+import useLogin from "../hooks/useLogin";
 
 const Login = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+  const { loading, login } = useLogin();
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    login(inputs.username, inputs.password);
+  };
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-md shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
@@ -9,17 +20,33 @@ const Login = () => {
           Login
           <span className="text-blue-500"> Chat App</span>
         </h1>
-        <form className="">
+        <form onSubmit={handleSubmit}>
           <div>
             <label className="input input-bordered flex items-center gap-2 bg-blue-100 text-black font-semibold">
               <FaUser />
-              <input type="text" className="grow " placeholder="Username" />
+              <input
+                type="text"
+                className="grow "
+                placeholder="Username"
+                value={inputs.username}
+                onChange={(e) =>
+                  setInputs({ ...inputs, username: e.target.value })
+                }
+              />
             </label>
           </div>
           <div>
             <label className="input input-bordered flex items-center gap-2 bg-blue-100 text-black font-semibold mt-4">
               <FaKey />
-              <input type="password" className="grow " placeholder="Password" />
+              <input
+                type="password"
+                className="grow "
+                placeholder="Password"
+                value={inputs.password}
+                onChange={(e) =>
+                  setInputs({ ...inputs, password: e.target.value })
+                }
+              />
             </label>
           </div>
 
@@ -31,8 +58,11 @@ const Login = () => {
             <span className="font-semibold">SIGN UP</span>
           </a>
           <div>
-            <button className="btn btn-block btn-sm mt-4 bg-blue-950 hover:bg-blue-900 ">
-              LOGIN
+            <button
+              className="btn btn-block btn-sm mt-4 bg-blue-950 hover:bg-blue-900 "
+              disabled={loading}
+            >
+              {loading ? "Loading..." : "LOGIN"}
             </button>
           </div>
         </form>
