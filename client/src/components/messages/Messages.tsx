55ec50c15ad4e2109a9aa1ffe4361d5cb23fb433
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-11-30 23:59:05 +0530

    message skeleton

--- client/src/components/messages/Messages.tsx
+++ client/src/components/messages/Messages.tsx
@@ -1,13 +1,22 @@
 import Message from "./Message";
 import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeleton/MessageSkeleton";
 
 const Messages = () => {
   const { messages, loading } = useGetMessages();
   return (
     <div className="px-4 flex-1 overflow-auto">
-      {messages.map((message) => (
-        <Message key={message.id} message={message} />
-      ))}
+      {!loading &&
+        messages.map((message) => (
+          <Message key={message.id} message={message} />
+        ))}
+      {!loading && messages.length === 0 && (
+        <p className="text-center text-white font-semibold pt-5">
+          Send message to start conversation
+        </p>
+      )}
+      {loading &&
+        [...Array(3)].map((_, index) => <MessageSkeleton key={index} />)}
     </div>
   );
 };
--- client/src/components/skeleton/MessageSkeleton.tsx
+++ client/src/components/skeleton/MessageSkeleton.tsx
@@ -1,5 +1,20 @@
 const MessageSkeleton = () => {
-  return <div></div>;
+  return (
+    <div className="">
+      <div className="flex gap-3 items-center">
+        <div className="skeleton bg-blue-400 opacity-60 w-10 h-10 rounded-full shrink-0"></div>
+        <div className="flex flex-col gap-1">
+          <div className="skeleton bg-blue-200 opacity-40  h-4 w-40"></div>
+          <div className="skeleton bg-blue-200 opacity-40 h-4 w-40"></div>
+        </div>
+      </div>
+      <div className="flex gap-3 items-center justify-end">
+        <div className="flex flex-col gap-1">
+          <div className="skeleton bg-blue-200 opacity-40 h-4 w-40"></div>
+        </div>
+        <div className="skeleton bg-blue-400 opacity-60  w-10 h-10 rounded-full shrink-0"></div>
+      </div>
+    </div>
+  );
 };
-
 export default MessageSkeleton;
